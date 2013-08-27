0af39b86dde0839c3b41af60a20ccc96643d665e
Matt Yeh <dev.mtyeh411@gmail.com>
2013-08-27 12:59:19 -0400

    uncomment authenticate user before_filters

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
   def show
     @trip = Trip.find_by_trip_id(params[:id])
 
